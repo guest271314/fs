a5ab0ea7b7669be97767c25c385e864aab244e80
guest271314 <guest271314@gmail.com>
2023-06-17 22:27:49 -0700

    Create watcher.js

---
+++ deno/watcher.js
@@ -0,0 +1,34 @@
+const file = 'input.js';
+
+console.log(`Watching ${file}`);
+
+for (;;) {
+  const watcher = Deno.watchFs('');
+  for await (const {
+      kind,
+      paths
+    }
+    of watcher) {
+    if (kind === 'modify' &&
+      paths.find((path) => new RegExp(`${file}$`).test(path))) {
+      // console.log(kind, paths);
+      const command = new Deno.Command(Deno.execPath(), {
+        args: [
+          "run",
+          "-A",
+          "input.js",
+        ],
+      });
+      const {
+        code,
+        stdout,
+        stderr
+      } = await command.output();
+      break;
+    }
+  }
+  try {
+    watcher.close();
+  } catch {}
+  continue;
+}
