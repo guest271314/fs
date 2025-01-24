0cbd4ca6b41fcd8a60ff7ebedd5d03891ae49454
guest271314 <guest271314@gmail.com>
2025-01-23 21:32:37 -0800

    Update fs.js

--- nodejs/fs.js
+++ nodejs/fs.js
@@ -1,46 +1,45 @@
-var [input, output] = await Promise.all(
-  ["stdin", "stdout"].map((stdio) =>
-    showSaveFilePicker({
-      startIn: "documents",
-      suggestedName: stdio,
-    })
-  ),
-);
+var dir = await showDirectoryPicker();
+var input = await dir.getFileHandle("stdin", {
+  create: true
+});
+var output = await dir.getFileHandle("stdout", {
+  create: true
+});
 
-const { readable: stdout, writable: stdin } = new TransformStream();
+const {
+  readable: stdout,
+  writable: stdin
+} = new TransformStream();
 
-stdout.pipeThrough(new TextDecoderStream()).pipeTo(
-  new WritableStream({
-    async write(value) {
-      console.log(value);
-      if (value === "Z") {
-        await new Blob([]).stream().pipeTo(await output.createWritable());
-      }
-    },
-  }),
-);
-
-var fso = new FileSystemObserver(
-  async ([{ changedHandle, root, type }], record) => {
-    try {
-      if (type === "modified") {
-        await new Response(await(await output.getFile()).arrayBuffer()).body.pipeTo(stdin, {
-          preventClose: true
-        });
-      }
-    } catch (e) {
-      console.warn(e);
+stdout.pipeThrough(new TextDecoderStream()).pipeTo(new WritableStream({
+  async write(value) {
+    console.log(value);
+    if (value === "Z") {
+      await new Blob([]).stream().pipeTo(await output.createWritable());
+      fso.unobserve(output);
+      fso.disconnect();
     }
   },
-);
+}));
+
+var fso = new FileSystemObserver(async ([{
+  changedHandle,
+  root,
+  type
+}], record) => {
+  try {
+    if (type === "modified") {
+      await new Response(await (await output.getFile()).arrayBuffer()).body.pipeTo(stdin, {
+        preventClose: true
+      });
+    }
+  } catch (e) {
+    console.warn(e);
+  }
+});
 
 fso.observe(output);
 
-[..."abcdefghijklmnopqrstuvwxyz"]
-  .reduce(
-    (a, b) =>
-      a.then(async () =>
-        new Blob([b]).stream().pipeTo(await input.createWritable())
-      ),
-    Promise.resolve(),
-  );
+[..."abcdefghijklmnopqrstuvwxyz"].reduce((a, b) => 
+  a.then(async () => new Blob([b]).stream().pipeTo(await input.createWritable()))
+, Promise.resolve());
